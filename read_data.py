bcba6bf7ce17e7a8eaf588710ab255f2bebeb840
Bruno <Bruno.Kaukars@gmx.net>
2025-05-12 15:51:08 +0200

    variable file path

--- read_data.py
+++ read_data.py
@@ -1,5 +1,5 @@
 from netCDF4 import Dataset
-rootgrp = Dataset(r"C:\Users\bruno\Downloads\S3A_SR_2_LAN_HY_20241020T203045_20241020T203859_20241116T003153_0494_118_171______PS1_O_NT_005.SEN3\standard_measurement.nc", "r", format="NETCDF4")
+rootgrp = Dataset(r"data\S3A_SR_2_LAN_HY_20241020T203045_20241020T203859_20241116T003153_0494_118_171______PS1_O_NT_005.SEN3\standard_measurement.nc", "r", format="NETCDF4")
 print(rootgrp.data_model)
 print("-------------------")
 print(rootgrp.variables.keys())
