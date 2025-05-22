6486bbf9ddea78bf45ae7c85aa53926f9c36361a
st172398 <st172398@stud.uni-stuttgart.de>
2025-05-23 00:41:50 +0200

    stuff

--- read_data.py
+++ read_data.py
@@ -1,8 +1,12 @@
 from netCDF4 import Dataset
 import numpy as np
+from numpy import array2string
 import matplotlib.pyplot as plt
 
+np.set_printoptions(threshold=np.inf)
+
 # Variables:
+# lat_20_ku (lat range: 52.334067 to 52.471998)
 # range_water_20_ku
 # mod_wet_tropo_cor_meas_altitude_01
 # mod_dry_tropo_cor_meas_altitude_01
@@ -22,17 +26,54 @@ def to_list(var):
     return a.tolist()
 
 
-range_w_temp = to_list("range_water_20_ku")
-range_w = []
-
-# What should we do with NaN cases? I will pop them from the list for now...
-for i in range(len(range_w_temp)):
-    if str(range_w_temp[i]) != "nan":
-        range_w.append(range_w_temp[i])
-
+range_w = to_list("range_water_20_ku")
+lat = to_list("lat_20_ku")
 wet_cor_01 = to_list("mod_wet_tropo_cor_meas_altitude_01")
 dry_cor_01 = to_list("mod_dry_tropo_cor_meas_altitude_01")
 iono_cor_01 = to_list("iono_cor_gim_01_ku")
+
+wet_cor_20 = np.linspace(0, 0, len(range_w))
+dry_cor_20 = np.linspace(0, 0, len(range_w))
+iono_cor_20 = np.linspace(0, 0, len(range_w))
+
+
+def create_list_20(data_01, total_len):
+    
+    empty_20 = np.linspace(0, 0, total_len)
+    num_steps = int(np.round(total_len/len(data_01), decimals=0))
+
+    # loop fills 
+    counter = 0
+    selector = 0
+
+    for i in range(len(empty_20)):
+        counter += 1
+
+        if counter == num_steps:
+            if selector >= len(data_01):
+                break
+            empty_20[i] = data_01[selector]
+            selector += 1
+            counter = 0
+    return empty_20
+
+wet_cor_20 = create_list_20(wet_cor_01, len(range_w))
+
+with open("range.txt", "w") as f:
+    f.write(str(range_w))
+
+with open("lat.txt", "w") as f:
+    f.write(str(lat))
+
+with open("wet_cor_20.txt", "w") as f:
+    f.write(str(wet_cor_20.tolist()))
+
+with open("dry_cor_01.txt", "w") as f:
+    f.write(str(dry_cor_01))
+
+
+
+
 
 # Problem: range is in 20GHz, all other variables in 1GHz
 # -> Add missing points through linear regression
@@ -92,19 +133,19 @@ def interpolate20(data):
 
 # First calculation: Corrected range = Range + Wet Tropo Correction + Dry Tropo Correction + Iono Correction
 
-wet_cor_20 = interpolate20(wet_cor_01)
-dry_cor_20 = interpolate20(dry_cor_01)
-iono_cor_20 = interpolate20(iono_cor_01)
+# wet_cor_20 = interpolate20(wet_cor_01)
+# dry_cor_20 = interpolate20(dry_cor_01)
+# iono_cor_20 = interpolate20(iono_cor_01)
 
-range_cor = []
+# range_cor = []
 
-for i in range(len(range_w)):
-    range_cor.append(range_w[i] + wet_cor_20[i] + dry_cor_20[i] + iono_cor_20[i])
+# # for i in range(len(range_w)):
+# #     range_cor.append(range_w[i] + wet_cor_20[i] + dry_cor_20[i] + iono_cor_20[i])
 
-# plot:
-x_axis = list(range(len(range_w)))
+# # plot:
+# x_axis = list(range(len(range_w)))
 
-plt.plot(x_axis, range_w, label="Range")
-plt.plot(x_axis, range_cor, label="Corrected Range")
+# plt.plot(x_axis, range_w, label="Range")
+# plt.plot(x_axis, range_cor, label="Corrected Range")
 
-plt.show()
+# plt.show()
